7bd9e37c9e610d1e2cf86ad9a6c1c8a189b65ea3
Felipe José de Lima <lplplimald@gmail.com>
2024-09-15 17:08:40 -0300

    first commit

---
+++ src/components/CampoDeBusca/index.jsx
@@ -0,0 +1,19 @@
+"use-client"
+import Image from 'next/image'
+import styles from "./CampoDeBusca.module.css";
+import Lupa from "../../../public/Assets/lupa.png"
+
+const CampoDeBusca = ({textoBuscaDigitado, handleBusca}) => {
+    return (
+    <div className={styles.container}>
+        <Image className={styles.icone} src={ Lupa } alt="icone"/>
+        <input
+        type='text'
+        value={textoBuscaDigitado}
+        onChange={(event) => handleBusca(event.target.value)}
+        placeholder='Pesquise aqui um de nossos pratos'/>
+    </div>
+    )
+}
+
+export default CampoDeBusca
---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,25 @@
+"use-client"
+import Image from 'next/image';
+import styles from './cards.module.css';
+
+const Cards = ({ produto }) => {
+    return(
+        <div className={styles.container}>
+            <figure>
+                <Image src={produto.imagem} alt={produto.nome} />
+            </figure>
+            <div className={styles.container_informacoes}>
+                <div>
+                    <h3>{produto.nome}</h3>
+                    <small>{produto.categoria}</small>
+                    <p>{produto.descricao}</p>
+                </div>
+                <div>
+                    <span>{new Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(produto.preco)}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
